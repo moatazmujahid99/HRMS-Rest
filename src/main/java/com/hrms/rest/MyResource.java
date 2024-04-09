1f4903e6ea5114d3041ccf517f3c08f6292a0ae4
moatazmujahid99 <forprofmoataz500@gmail.com>
2024-04-09 10:52:55 +0200

    build the entities

--- src/main/java/com/hrms/rest/MyResource.java
+++ src/main/java/com/hrms/rest/MyResource.java
@@ -1,9 +1,17 @@
 package com.hrms.rest;
 
+import com.hrms.rest.persistence.dept;
+import com.hrms.rest.persistence.entity.Employee;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 
 /**
  * Root resource (exposed at "myresource" path)
@@ -18,8 +26,17 @@ public class MyResource {
      * @return String that will be returned as a text/plain response.
      */
     @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String getIt() {
-        return "Got it!";
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getIt() {
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("hrms");
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        Employee emp = entityManager.find(Employee.class, 1);
+        dept d=new dept();
+        d.id=emp.getManagedDepartment().getDepartmentId();
+        d.name=emp.getManagedDepartment().getDepartmentName();
+        System.out.println(emp.getManagedDepartment());
+    
+        return Response.ok(d).build();
     }
+    
 }
---
+++ src/main/java/com/hrms/rest/persistence/dept.java
@@ -0,0 +1,8 @@
+package com.hrms.rest.persistence;
+
+import java.io.Serializable;
+
+public class dept implements Serializable {
+    public int id;
+    public String name;
+}
