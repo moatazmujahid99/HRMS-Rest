32e2cb3c7e7354f0a6eaa1157c1968bd6e5be36d
moatazmujahid99 <forprofmoataz500@gmail.com>
2024-04-10 18:51:25 +0200

    remove unnecessary dependencies and uncomment GenericExceptionMapper

--- src/main/java/com/hrms/rest/presentation/exceptionMapper/GenericExceptionMapper.java
+++ src/main/java/com/hrms/rest/presentation/exceptionMapper/GenericExceptionMapper.java
@@ -1,22 +1,22 @@
-// package com.hrms.rest.presentation.exceptionMapper;
+package com.hrms.rest.presentation.exceptionMapper;
 
-// import com.hrms.rest.presentation.dto.ErrorMessageDTO;
+import com.hrms.rest.presentation.dto.ErrorMessageDTO;
 
-// import jakarta.ws.rs.core.Response;
-// import jakarta.ws.rs.ext.ExceptionMapper;
-// import jakarta.ws.rs.ext.Provider;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.ext.ExceptionMapper;
+import jakarta.ws.rs.ext.Provider;
 
-// @Provider
-// public class GenericExceptionMapper implements ExceptionMapper<Throwable> {
+@Provider
+public class GenericExceptionMapper implements ExceptionMapper<Throwable> {
 
-//     @Override
-//     public Response toResponse(Throwable exception) {
-//         ErrorMessageDTO error = ErrorMessageDTO.builder()
-//                 .message(exception.getMessage())
-//                 .errorCode(500)
-//                 .build();
+    @Override
+    public Response toResponse(Throwable exception) {
+        ErrorMessageDTO error = ErrorMessageDTO.builder()
+                .message(exception.getMessage())
+                .errorCode(500)
+                .build();
 
-//         return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(error).build();
-//     }
+        return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(error).build();
+    }
 
-// }
+}
